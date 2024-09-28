2c1d761e2cedbcb5344acb5cf0960141f1e5cd2a
Alex Gomes <alex.gsone@gmail.com>
2024-09-28 18:19:40 -0300

    Update LoginController.java

--- BarberShopCurso/src/Controller/LoginController.java
+++ BarberShopCurso/src/Controller/LoginController.java
@@ -16,43 +16,39 @@ import View.MenuPrincipal;
  * @author alex
  */
 public class LoginController {
-    
+
     private final Login view;
     private LoginHelper helper;
-    
+
     public LoginController(Login view) {
         this.view = view;
         this.helper = new LoginHelper(view);
     }
-    
+
     public void entrarNoSistema() {
 //      Pegar um usuário da view
         Usuario usuario = helper.obterModelo();
 
 //      Pesquisar  Usuário no Banco
         UsuarioDAO usuarioDAO = new UsuarioDAO();
-        Usuario usuarioAutenticado =  usuarioDAO.selectPorNomeESenha(usuario);
-        
+        Usuario usuarioAutenticado = usuarioDAO.selectPorNomeESenha(usuario);
+
 //      Se o usuário da view tiver o mesmo usuário e senha que o usuário do vindo do banco, direcionar para o menu.
 //      Se não mostrar uma mensagem so usuário "Usuário ou senha inválidos".
-        if(usuarioAutenticado != null){
-            
+        if (usuarioAutenticado != null) {
+
 //          navegar para menu principal
             MenuPrincipal menu = new MenuPrincipal();
             menu.setVisible(true);
             this.view.dispose();
-        }else{
+        } else {
             view.exibeMensagem("Usuário ou senha inválidos");
         }
-
-
-        
     }
-    
+
     public void fizTarefa() {
         System.out.println("Busquei algo do banco de dados");
-        
+
         this.view.exibeMensagem("Executei o fiz tarefa");
     }
-    
 }
