a34a7e876ed70f0211209be366ef9714dbfd9bb7
Alex Gomes <alex.gsone@gmail.com>
2024-10-09 23:57:27 -0300

    Update Correio.java

--- BarberShopCurso/src/Servico/Correio.java
+++ BarberShopCurso/src/Servico/Correio.java
@@ -37,7 +37,7 @@ public class Correio {
         float valor = agendamento.getValor();
         
         return "Olá "+ nomeCliente + " Vai dar um tapa no visu é... Seu agendamento para "+ servico+", esta marcado para dia "+ dataAgendamento
-                +" às "+ horaAgendamento + "\n\n O preço para você sai baratin fica R$"+ valor + " \n\nGrande Abraço!!";
+                +" às "+ horaAgendamento + "\n\n O preço para você sai baratin fica R$"+ valor + " \n\nForte Abraço!!";
     }
     
 }
