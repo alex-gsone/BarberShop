a71eec02dae6214e66f3363e80cd9d537019c638
Alex Gomes <alex.gsone@gmail.com>
2024-10-10 23:10:56 -0300

    Update Correio.java

--- BarberShopCurso/src/Servico/Correio.java
+++ BarberShopCurso/src/Servico/Correio.java
@@ -8,26 +8,29 @@ package Servico;
 import Model.Agendamento;
 
 /**
- * Serviço Responsavel por manipulas as saidas de email ou notificacoes  do sistema
+ * Serviço Responsavel por manipulas as saidas de email ou notificacoes do
+ * sistema
+ *
  * @author tiago
  */
 public class Correio {
-    
-    public void NotificarPorEmail(Agendamento agendamento){
-    
+
+    public void NotificarPorEmail(Agendamento agendamento) {
+
         String emailFormatado = formatarEmail(agendamento);
         String destinatario = agendamento.getCliente().getEmail();
-        
+
         //Usando Classe Email para fazer os envios
-        Email email = new Email("Agandamento BarberShop",emailFormatado,destinatario);
+        Email email = new Email("Agandamento BarberShop", emailFormatado, destinatario);
         email.enviar();
-        
+
     }
 
     /**
      * Função auxiliar para formatar o texto do Email
+     *
      * @param agendamento
-     * @return 
+     * @return
      */
     private String formatarEmail(Agendamento agendamento) {
         String nomeCliente = agendamento.getCliente().getNome();
@@ -35,9 +38,9 @@ public class Correio {
         String dataAgendamento = agendamento.getDataFormatada();
         String horaAgendamento = agendamento.getHoraFormatada();
         float valor = agendamento.getValor();
-        
-        return "Olá "+ nomeCliente + " Vai dar um tapa no visu é... Seu agendamento para "+ servico+", esta marcado para dia "+ dataAgendamento
-                +" às "+ horaAgendamento + "\n\n O preço para você sai baratin fica R$"+ valor + " \n\nForte Abraço!!";
+
+        return "Olá " + nomeCliente + " Vai dar um tapa no visu é... Seu agendamento para " + servico + ", esta marcado para dia " + dataAgendamento
+                + " às " + horaAgendamento + "\n\n O preço para você sai baratin fica R$" + valor + " \n\nForte Abraço!!";
     }
-    
+
 }
